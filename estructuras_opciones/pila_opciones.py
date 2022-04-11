0ec62cb0b06f83c65e714e55593e93f817130068
Limbers <limbermay1@gmail.com>
2022-04-11 18:32:47 -0500

    se agregó la interfaz de pila_opciones

--- estructuras_opciones/pila_opciones.py
+++ estructuras_opciones/pila_opciones.py
@@ -1,6 +1,18 @@
 from tkinter import *
+from frontend.pila_interfaz import PilaInterfaz
 
 class PilaOpciones(Frame):
 
     def __init__(self, master):
         Frame.__init__(self, master)
+    
+        master.title("Estructura de datos Pila")
+
+        # Elementos del frame
+        self.label_titulo = Label(self, text="Pila")
+
+        self.pila_interfaz = PilaInterfaz(self)
+
+        # Posicionamiento de los elementos
+        self.label_titulo.grid(row=0, column=0)
+        self.pila_interfaz.grid(row=1, column=0)
