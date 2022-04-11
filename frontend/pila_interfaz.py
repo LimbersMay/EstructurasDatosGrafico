14e729c6a0eeeffd1062637af8ed5c8d42acd081
Limbers <limbermay1@gmail.com>
2022-04-11 16:53:00 -0500

    se agregó la función de dibujar pila

--- frontend/pila_interfaz.py
+++ frontend/pila_interfaz.py
@@ -12,6 +12,7 @@ class PilaInterfaz(Frame):
         self.config(
             width = 400,
             height = 300,
+            bg = 'brown',
         )
 
         self.grid_propagate(False)
@@ -25,4 +26,21 @@ class PilaInterfaz(Frame):
 
 
         # Posicionamiento de los elementos
-    
+
+    def dibujar_pila(self):
+
+        self.pila.insertar(5)
+
+        elementos_pila = self.pila.recorrer()
+
+        # Creamos un frame por cada elemento
+        for i in range(len(elementos_pila)):
+
+            # Creamos un frame
+            frame = Frame(self)
+
+            # Posicionamiento del frame
+            frame.grid(row=i, column=0)
+
+            # Insertamos el elemento en el frame
+            Label(frame, text=elementos_pila[i]).pack()
