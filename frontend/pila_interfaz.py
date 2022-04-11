0f6aaa48d3d722a8c3b615db261fe1a26ed112cc
Limbers <limbermay1@gmail.com>
2022-04-11 16:12:02 -0500

    se agregó la configuración de la interfaz de la pila

--- frontend/pila_interfaz.py
+++ frontend/pila_interfaz.py
@@ -1,6 +1,15 @@
 from tkinter import *
+from estructuras.pila.pila import Pila
 
 class PilaInterfaz(Frame):
 
     def __init__(self, master):
         Frame.__init__(self, master)
+
+        # Configuraciones de la ventana
+        self.config(
+            width = 400,
+            height = 300,
+        )
+
+        self.grid_propagate(False)
