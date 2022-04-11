c922be5fbd697e9fa39338d768adf308aa1309a7
Limbers <limbermay1@gmail.com>
2022-04-11 18:01:37 -0500

    se acualizó el main

---
+++ main.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from ventana_principal import *
+
+def main():
+
+    root = Tk()
+    ventana_principal = Ventana(root)
+    ventana_principal.pack()
+    root.mainloop()
+
+main()
