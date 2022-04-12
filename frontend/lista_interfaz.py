29b49bc9a083f974c99c43bdb03b213819721dda
Limbers <limbermay1@gmail.com>
2022-04-11 22:26:07 -0500

    se agregó la función para dibujar la lista

--- frontend/lista_interfaz.py
+++ frontend/lista_interfaz.py
@@ -1,4 +1,6 @@
 from tkinter import *
+
+from matplotlib.pyplot import text
 from estructuras.lista_enlazada.linked_list import LinkedList
 
 class ListaInterfaz(Frame):
@@ -9,4 +11,27 @@ class ListaInterfaz(Frame):
         self.lista = LinkedList()
 
         # Configuraciones de la ventana
-        self.config(width=300, height=300)
+        self.config(width=800, height=350)
+        self.grid_propagate(False)
+
+        self.rowconfigure(0, weight=1)
+    
+    def dibujar_lista(self):        
+        lista_frames = []
+
+        # Por cada nodo de la lista, se crea un frame
+        for i in range(self.lista._size):
+            nuevo_frame = Frame(self, width=50, height=50, bg='red', highlightbackground="black", highlightthickness=1)
+            lista_frames.append(nuevo_frame)
+            
+            # Se agrega el texto y la referencia del nodo al frame
+            texto = Label(nuevo_frame, text=self.lista.search_position_node(i).data, bg='red')
+            referencia = Label(nuevo_frame, text=id(self.lista.search_position_node(i)), bg='red')
+            
+            # Los empaquetamos
+            texto.grid(row=0, column=0)
+            referencia.grid(row=1, column=0)
+        
+        # Posicionamos los frames en el medio de la ventana, uno a la derecha del otro
+        for i in range(len(lista_frames)):
+            lista_frames[i].grid(row=0, column=i, sticky=W)
