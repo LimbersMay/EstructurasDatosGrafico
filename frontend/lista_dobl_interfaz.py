637fbc311775705540f6a4bcf9161f3892d474db
Limbers <limbermay1@gmail.com>
2022-04-12 09:23:21 -0500

    Se agregó la configuración principal

--- frontend/lista_dobl_interfaz.py
+++ frontend/lista_dobl_interfaz.py
@@ -1,6 +1,16 @@
 from tkinter import *
+from estructuras.lista_dob_enlazada.double_linked_list import DoubleLinkedList
 
 class ListaDobEnlazadaInterfaz(Frame):
 
     def __init__(self, master):
         Frame.__init__(self, master)
+
+        self.lista = DoubleLinkedList()
+        self.lista_frames = []
+
+        # Configuraciones de la ventana
+        self.config(width=800, height=350, bg="#146356")
+        self.grid_propagate(False)
+
+        self.rowconfigure(0, weight=1)
