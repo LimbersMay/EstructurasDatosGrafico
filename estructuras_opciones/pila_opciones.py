a3356eac9e8101b2ae645676defe1d48fec50fea
Limbers <limbermay1@gmail.com>
2022-04-11 20:57:29 -0500

    se agregó la clase BotonesInferiores para el frame inferior

--- estructuras_opciones/pila_opciones.py
+++ estructuras_opciones/pila_opciones.py
@@ -10,9 +10,32 @@ class PilaOpciones(Frame):
 
         # Elementos del frame
         self.label_titulo = Label(self, text="Pila")
-
         self.pila_interfaz = PilaInterfaz(self)
+        self.botones_inferiores = BotonesInferiores(self)
 
         # Posicionamiento de los elementos
         self.label_titulo.grid(row=0, column=0)
         self.pila_interfaz.grid(row=1, column=0)
+        self.botones_inferiores.grid(row=2, column=0)
+
+
+class BotonesInferiores(Frame):
+
+    def __init__(self, master):
+        Frame.__init__(self, master)
+
+        # Elementos del frame
+        self.texto_dato = Label(self, text="Introduce un valor: ")
+        self.dato_entru = Entry(self)
+
+        self.insertar_button = Button(self, text="Insertar")
+        self.eliminar_button = Button(self, text="Eliminar")
+        self.buscar_button = Button(self, text="Buscar")
+
+        # Posicionamiento de los elementos
+        self.texto_dato.grid(row=0, column=0)
+        self.dato_entru.grid(row=0, column=1)
+
+        self.insertar_button.grid(row=0, column=2)
+        self.eliminar_button.grid(row=0, column=3)
+        self.buscar_button.grid(row=0, column=4)
