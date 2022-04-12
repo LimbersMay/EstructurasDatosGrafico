be14b17341dc8e1daa581a18cd354bd8543b71d6
Limbers <limbermay1@gmail.com>
2022-04-12 08:42:06 -0500

    se agregaron los elementos básicos del frame (titulo, lista_interfaz, botones_inferiores)

--- estructuras_opciones/lista_opciones.py
+++ estructuras_opciones/lista_opciones.py
@@ -1,6 +1,23 @@
 from tkinter import *
+from frontend.lista_interfaz import ListaInterfaz
 
 class ListaOpciones(Frame):
 
     def __init__(self, master):
+        Frame.__init__(self, master)
+
+        master.title("Estructura de datos Lista")
+
+        # Elementos del frame
+        self.titulo = Label(self, text="Lista simplemente enlazada")
+        self.lista_interfaz = ListaInterfaz(self)
+        self.botones_inferiores = BotonesInferiores(self, self.lista_interfaz)
+
+        # Posicionamiento de los elementos
+        self.titulo.grid(row=0, column=0)
+        self.lista_interfaz.grid(row=1, column=0)
+        self.botones_inferiores.grid(row=2, column=0)
+
+class BotonesInferiores(Frame):
+    def __init__(self, master, lista_interfaz):
         Frame.__init__(self, master)
