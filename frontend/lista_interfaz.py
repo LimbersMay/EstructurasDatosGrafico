890742e803567f0d3277d1068cdd299abcb40514
Limbers <limbermay1@gmail.com>
2022-04-11 21:48:18 -0500

    se agregó la configuración básica de la lista

--- frontend/lista_interfaz.py
+++ frontend/lista_interfaz.py
@@ -1,6 +1,12 @@
 from tkinter import *
+from estructuras.lista_enlazada.linked_list import LinkedList
 
 class ListaInterfaz(Frame):
 
     def __init__(self, master):
         Frame.__init__(self, master)
+
+        self.lista = LinkedList()
+
+        # Configuraciones de la ventana
+        self.config(width=300, height=300)
