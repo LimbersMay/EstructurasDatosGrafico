5ab8f47d3f88e810df2639bc7bce64b6c96b82bf
Limbers <limbermay1@gmail.com>
2022-04-11 21:41:04 -0500

    se agregaron las operaciones de insertar, eliminar y buscar en la ventana de opciones

--- estructuras_opciones/pila_opciones.py
+++ estructuras_opciones/pila_opciones.py
@@ -11,7 +11,7 @@ class PilaOpciones(Frame):
         # Elementos del frame
         self.label_titulo = Label(self, text="Pila")
         self.pila_interfaz = PilaInterfaz(self)
-        self.botones_inferiores = BotonesInferiores(self)
+        self.botones_inferiores = BotonesInferiores(self, self.pila_interfaz)
 
         # Posicionamiento de los elementos
         self.label_titulo.grid(row=0, column=0)
@@ -21,21 +21,33 @@ class PilaOpciones(Frame):
 
 class BotonesInferiores(Frame):
 
-    def __init__(self, master):
+    def __init__(self, master, pila_interfaz):
         Frame.__init__(self, master)
+
+        # Recibimos la pila interfaz para poder acceder a los métodos de eliminar, insertar y demás
+        self.pila_interfaz = pila_interfaz
 
         # Elementos del frame
         self.texto_dato = Label(self, text="Introduce un valor: ")
-        self.dato_entru = Entry(self)
+        self.dato_entry = Entry(self)
 
-        self.insertar_button = Button(self, text="Insertar")
-        self.eliminar_button = Button(self, text="Eliminar")
-        self.buscar_button = Button(self, text="Buscar")
+        self.insertar_button = Button(self, text="Insertar", command=self.insertar)
+        self.eliminar_button = Button(self, text="Eliminar", command=self.eliminar)
+        self.buscar_button = Button(self, text="Buscar", command=self.buscar)
 
         # Posicionamiento de los elementos
         self.texto_dato.grid(row=0, column=0)
-        self.dato_entru.grid(row=0, column=1)
+        self.dato_entry.grid(row=0, column=1)
 
         self.insertar_button.grid(row=0, column=2)
         self.eliminar_button.grid(row=0, column=3)
         self.buscar_button.grid(row=0, column=4)
+    
+    def insertar(self):
+        self.pila_interfaz.insertar(self.dato_entry.get())
+    
+    def eliminar(self):
+        self.pila_interfaz.eliminar()
+    
+    def buscar(self):
+        self.pila_interfaz.buscar(self.dato_entry.get())
