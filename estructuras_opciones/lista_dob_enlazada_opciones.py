37a82300e9f4942c508b24c39da989c17a783a24
Limbers <limbermay1@gmail.com>
2022-04-13 10:49:32 -0500

    se agregó la función de eliminar por posición

--- estructuras_opciones/lista_dob_enlazada_opciones.py
+++ estructuras_opciones/lista_dob_enlazada_opciones.py
@@ -33,6 +33,8 @@ class BotonesInferiores(BotonesLista):
 
         self.insertar_posicion_button = Button(self, text="Insertar en posicion", command=self.insertar_posicion)
 
+        self.eliminar_posicion_button = Button(self, text="Eliminar en posicion", command=self.eliminar_posicion)
+
         # Posicionamiento de los elementos
         self.dato_label.grid(row=0, column=0)
         self.dato_entry.grid(row=0, column=1)
@@ -41,15 +43,19 @@ class BotonesInferiores(BotonesLista):
         self.indice_entry.grid(row=0, column=3)
 
         self.insertar_posicion_button.grid(row=0, column=4)
+        self.eliminar_posicion_button.grid(row=0, column=5)
 
-        self.insertar_final.grid(row=0, column=5)
-        self.insertar_inicio.grid(row=0, column=6)
+        self.insertar_final.grid(row=0, column=6)
+        self.insertar_inicio.grid(row=0, column=7)
 
-        self.eliminar_final.grid(row=0, column=7)
-        self.eliminar_inicio.grid(row=0, column=8)
+        self.eliminar_final.grid(row=0, column=8)
+        self.eliminar_inicio.grid(row=0, column=9)
 
-        self.buscar_button.grid(row=0, column=9)
+        self.buscar_button.grid(row=0, column=10)
     
     # Métodos de la lista
     def insertar_posicion(self):
         self.lista_interfaz.insertar_posicion(self.dato_entry.get(), int(self.indice_entry.get()))
+    
+    def eliminar_posicion(self):
+        self.lista_interfaz.eliminar_por_posicion(int(self.indice_entry.get()))
