1f9cfea41ac22021b44de79e05ffcafb26960109
Limbers <limbermay1@gmail.com>
2022-04-13 09:56:01 -0500

    Se agregó el método para rotar la lista

--- frontend/lista_circular_interfaz.py
+++ frontend/lista_circular_interfaz.py
@@ -4,3 +4,11 @@ class ListaCircularInterfaz(Frame):
 
     def __init__(self, master):
         Frame.__init__(self, master)
+    
+
+    # Métodos de la lista circular
+    def rotar_izquierda(self):
+        pass
+
+    def rotar_derecha(self):
+        pass
