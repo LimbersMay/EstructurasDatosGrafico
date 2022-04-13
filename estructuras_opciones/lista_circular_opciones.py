3a2e5e773ebe7b4d8001586cea49774fae022365
Limbers <limbermay1@gmail.com>
2022-04-12 23:54:13 -0500

    se agregaron los elementos básicos de la lista circular

--- estructuras_opciones/lista_circular_opciones.py
+++ estructuras_opciones/lista_circular_opciones.py
@@ -1,6 +1,19 @@
 from tkinter import *
+from estructuras_opciones.botones_inf_listas import PlantillaBotones
+from frontend.lista_circular_interfaz import ListaCircularInterfaz
 
 class ListaCircularOpciones(Frame):
 
     def __init__(self, master):
         Frame.__init__(self, master)
+
+        master.title("Lista Circular")
+
+        # Elementos del frame
+        self.titulo = Label(self, text="Lista Circular")
+        self.lista_interfaz = ListaCircularInterfaz(self)
+        self.botones_inferiores = BotonesInferiores(self, self.lista_interfaz)
+
+class BotonesInferiores(PlantillaBotones):
+    def __init__(self, master, lista_interfaz):
+        super().__init__(master, lista_interfaz)
