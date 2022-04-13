4ae91e70bfe666da93b90931c57c4d2eec03c960
Limbers <limbermay1@gmail.com>
2022-04-13 13:29:09 -0500

    Se agregó la interfaz de la cola

--- frontend/cola_interfaz.py
+++ frontend/cola_interfaz.py
@@ -1,6 +1,9 @@
-from tkinter import *
+from frontend.lista_interfaz import ListaInterfaz
+from estructuras.cola.cola import Cola
 
-class ColaInterfaz(Frame):
+class ColaInterfaz(ListaInterfaz):
 
     def __init__(self, master):
-        Frame.__init__(self, master)
+        super().__init__(master)
+
+        self.cola = Cola()
