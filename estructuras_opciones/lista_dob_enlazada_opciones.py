b90c0f5a3315a86776b34e69b2ff6611fc894c9a
Limbers <limbermay1@gmail.com>
2022-04-12 19:21:43 -0500

    se agregaron los elementos principales (titulo, lista_doble y botones_inferiores)

--- estructuras_opciones/lista_dob_enlazada_opciones.py
+++ estructuras_opciones/lista_dob_enlazada_opciones.py
@@ -1,6 +1,19 @@
 from tkinter import *
+from frontend.lista_dobl_interfaz import ListaDobleInterfaz
 
 class ListaDobEnOpciones(Frame):
 
     def __init__(self, master):
         Frame.__init__(self, master)
+
+        master.title("Lista Doble Enlazada")
+
+        # Elementos del frame
+        self.titulo = Label(self, text="Lista Doble Enlazada")
+        self.lista_doble_interfaz = ListaDobleInterfaz(self)
+        self.botones_inferiores = BotonesInferiores(self, self.lista_doble_interfaz)
+
+
+class BotonesInferiores(Frame):
+    def __init__(self, lista_doble_interfaz):
+        pass
