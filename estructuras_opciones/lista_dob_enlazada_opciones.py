7cccd8d3484abb9ef9c3267136b9052e53e850ba
Limbers <limbermay1@gmail.com>
2022-04-12 19:22:49 -0500

    se posicionaron los elementos

--- estructuras_opciones/lista_dob_enlazada_opciones.py
+++ estructuras_opciones/lista_dob_enlazada_opciones.py
@@ -13,6 +13,11 @@ class ListaDobEnOpciones(Frame):
         self.lista_doble_interfaz = ListaDobleInterfaz(self)
         self.botones_inferiores = BotonesInferiores(self, self.lista_doble_interfaz)
 
+        # Posicionamiento de los elementos
+        self.titulo.grid(row=0, column=0)
+        self.lista_doble_interfaz.grid(row=1, column=0)
+        self.botones_inferiores.grid(row=2, column=0)
+
 
 class BotonesInferiores(Frame):
     def __init__(self, lista_doble_interfaz):
