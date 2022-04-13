715fd2716776d4765cf645bd97af7b1a61fbfd1e
Limbers <limbermay1@gmail.com>
2022-04-13 11:11:50 -0500

    se agregó la configuración básica de la interfaz de lista circular

--- frontend/lista_circular_interfaz.py
+++ frontend/lista_circular_interfaz.py
@@ -1,11 +1,14 @@
 from tkinter import *
+from estructuras.lista_circular.circular_list import CircularList
+from frontend.lista_interfaz import ListaInterfaz
 
-class ListaCircularInterfaz(Frame):
+class ListaCircularInterfaz(ListaInterfaz):
 
     def __init__(self, master):
         Frame.__init__(self, master)
-    
 
+        self.lista = CircularList()
+    
     # Métodos de la lista circular
     def rotar_izquierda(self):
         pass
