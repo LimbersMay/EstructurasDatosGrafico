9b738e883c0fc158eabd221ea80b7337b7effe7a
Limbers <limbermay1@gmail.com>
2022-04-14 12:24:25 -0500

    Se refactorizó la clase BotonesInferiores de la clase lista_opciones

--- estructuras_opciones/lista_opciones.py
+++ estructuras_opciones/lista_opciones.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from estructuras_opciones.botones_template import BotonesLista
 from frontend.lista_simple_interfaz import ListaSimpleInterfaz
 
 
@@ -20,27 +21,11 @@ class ListaOpciones(Frame):
         self.botones_inferiores.grid(row=2, column=0)
 
 
-class BotonesInferiores(Frame):
+class BotonesInferiores(BotonesLista):
     def __init__(self, master, lista_interfaz):
-        Frame.__init__(self, master)
+        super().__init__(master, lista_interfaz)
 
-        # Recibimos la lista interfaz para poder acceder a los métodos de eliminar, insertar y demás
-        self.lista_interfaz = lista_interfaz
-
-        # Elementos del frame
-        self.texto_dato = Label(self, text="Introduce un valor: ")
-        self.dato_entry = Entry(self)
-
-        self.insertar_final = Button(self, text="Insertar al final", command=self.insertar_final)
-        self.insertar_inicio = Button(self, text="Insertar al inicio", command=self.insertar_inicio)
-
-        self.eliminar_final = Button(self, text="Eliminar al final", command=self.eliminar_final)
-        self.eliminar_inicio = Button(self, text="Eliminar al inicio", command=self.eliminar_inicio)
-
-        self.buscar_button = Button(self, text="Buscar", command=self.buscar)
-
-        # Posicionamiento de los elementos
-        self.texto_dato.grid(row=0, column=0)
+        self.dato_label.grid(row=0, column=0)
         self.dato_entry.grid(row=0, column=1)
 
         self.insertar_final.grid(row=0, column=2)
@@ -50,18 +35,3 @@ class BotonesInferiores(Frame):
         self.eliminar_inicio.grid(row=0, column=5)
 
         self.buscar_button.grid(row=0, column=6)
-
-    def insertar_final(self):
-        self.lista_interfaz.insertar_final(self.dato_entry.get())
-
-    def insertar_inicio(self):
-        self.lista_interfaz.insertar_inicio(self.dato_entry.get())
-
-    def eliminar_final(self):
-        self.lista_interfaz.eliminar_final()
-
-    def eliminar_inicio(self):
-        self.lista_interfaz.eliminar_inicio()
-
-    def buscar(self):
-        self.lista_interfaz.buscar(self.dato_entry.get())
