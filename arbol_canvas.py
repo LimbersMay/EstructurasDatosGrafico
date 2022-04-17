b38153b1ea2c60d8625ebcebd25e0b427b40f334
Limbers <limbermay1@gmail.com>
2022-04-17 18:22:33 -0500

    Se comprobó si un nodo tiene o no hijos y se procedió en base a eso

--- arbol_canvas.py
+++ arbol_canvas.py
@@ -20,6 +20,9 @@ class Arbol(Frame):
         self.diagonal_derecha = ImageTk.PhotoImage(Image.open("recursos/derecha.png").resize((40, 40)))
         self.diagonal_izquierda = ImageTk.PhotoImage(Image.open("recursos/izquierda.png").resize((40, 40)))
 
+        self.recta_derecha = ImageTk.PhotoImage(Image.open("recursos/recta_derecha.png").resize((40, 40)))
+        self.recta_izquierda = ImageTk.PhotoImage(Image.open("recursos/recta_izquierda.png").resize((40, 40)))
+
         self.arbol = BinaryTree(5)
 
     def dibujar_matriz(self):
@@ -51,10 +54,10 @@ class Arbol(Frame):
         # Agregamos nodos ficticios
         self.arbol.insert_left(3, 5)
         self.arbol.insert_right(7, 5)
-
-        # 7
         self.arbol.insert_left(2, 7)
         self.arbol.insert_right(6, 7)
+        self.arbol.insert_right(15, 6)
+        self.arbol.insert_left(15, 3)
 
         self.arbol.complete_tree()
 
@@ -76,42 +79,69 @@ class Arbol(Frame):
     def dibujar_raiz(self, profundidad_y: int, nodo):
         
         punto_medio_x = int(round(self.columnas / 2))
-        self.dibujar_nodo(nodo[0], profundidad_y, 0)
+        self.dibujar_nodo(nodo[0], profundidad_y, punto_medio_x)
 
-        # Pintamos 2 flechas en la diagonal derecha e izquierda
-        for i in range(1, 3):
-            diagonal_izquierda = self.matriz_frames[profundidad_y + i][punto_medio_x - i]
-            diagonal_derecha = self.matriz_frames[profundidad_y + i][punto_medio_x + i]
+        # Pintamos 2 flechas en la diagonal izquierda
+        self.dibujar_flecha_izquierda(profundidad_y, punto_medio_x, self.recta_izquierda)
+        self.dibujar_flecha_izquierda(profundidad_y + 1, punto_medio_x - 1)
 
-            Label(diagonal_izquierda, image=self.diagonal_izquierda, bg="#141E27").pack()
-            Label(diagonal_derecha, image=self.diagonal_derecha, bg="#141E27").pack()
+        # Pintamos 2 flechas en la diagonal derecha
+        self.dibujar_flecha_derecha(profundidad_y, punto_medio_x, self.recta_derecha)
+        self.dibujar_flecha_derecha(profundidad_y + 1, punto_medio_x + 1)
 
     # Función para insertar nodos
     def dibujar_nivel(self, profundidad_y: int, separacion_x: int, nodos: list):
-        print("Nodos: ", nodos)
+        punto_medio = int(round(self.columnas / 2))
 
         # Por la cantidad de nodos que tiene el nivel
-        for i in range(0, len(nodos) + 1, 4):
+        for i in range(0, len(nodos), 2):
+
+            auxiliar = i * 2
 
             # Obtenemos el nodo izquierdo y derecho
             nodo_izquierdo = nodos[0]
             nodo_derecho = nodos[-1]
 
+            sep_izquierdo = punto_medio - separacion_x + auxiliar
+            sep_derecho = punto_medio + separacion_x - auxiliar
+
             if nodo_izquierdo.get_data() is not None:
-                self.dibujar_nodo(nodo_izquierdo, profundidad_y, -separacion_x + i)
-                
+
+                self.dibujar_nodo(nodo_izquierdo, profundidad_y, sep_izquierdo)
+
+                # Comprobamos que el nodo tenga algún hijo
+                if not nodo_izquierdo.is_leaf():
+
+                    # Comprobamos que el nodo izquierdo tenga hijos
+                    if nodo_izquierdo.get_left().get_data() is not None:
+                        self.dibujar_flecha_izquierda(profundidad_y, sep_izquierdo)
+                    
+                    # Comprobamos que el nodo derecho tenga hijos
+                    if nodo_izquierdo.get_right().get_data() is not None:
+                        self.dibujar_flecha_derecha(profundidad_y, sep_izquierdo)
 
             if nodo_derecho.get_data() is not None:
-                self.dibujar_nodo(nodo_derecho, profundidad_y, separacion_x - i)
+
+                self.dibujar_nodo(nodo_derecho, profundidad_y, sep_derecho)
+
+                # Si este nodo no tiene ningún hijo
+                if not nodo_derecho.is_leaf():
+
+                    # Comprobamos si tiene un hijo derecho
+                    if nodo_derecho.get_right().get_data() is not None:
+                        self.dibujar_flecha_derecha(profundidad_y, sep_derecho)
+    
+                    # Comprobamos si tiene un hijo izquierdo
+                    if nodo_derecho.get_left().get_data() is not None:
+                        self.dibujar_flecha_izquierda(profundidad_y, sep_derecho)
             
             # Eliminamos los nodos
             nodos.pop()
             nodos.pop(0)
     
     # Función para insertar UN NODO en la matriz
-    def dibujar_nodo(self, nodo, profundidad_y, separacion_x):
-        punto_medio = int(round(self.columnas / 2))
-        frame_buscado = self.matriz_frames[profundidad_y][punto_medio + separacion_x]
+    def dibujar_nodo(self, nodo, coordenada_y, coordenada_x):
+        frame_buscado = self.matriz_frames[coordenada_y][coordenada_x]
 
         # Cambiamos de color al frame
         frame_buscado.config(bg='#FFC107')
@@ -124,14 +154,23 @@ class Arbol(Frame):
         Label(frame_buscado, text=nodo_dato, bg='#FFC107').pack()
         Label(frame_buscado, text=id_nodo, bg='#FFC107', font=("Helvetica", 7)).pack()
 
-
-    def dibujar_flecha_derecha(self, coordenada_x: int, coordenada_y: int):
+    def dibujar_flecha_derecha(self, coordenada_y: int, coordenada_x: int, imagen=None):
         frame_buscado = self.matriz_frames[coordenada_y + 1][coordenada_x + 1]
-        Label(frame_buscado, image=self.diagonal_derecha, bg="#141E27").pack()
+        flecha = Label(frame_buscado, image=self.diagonal_derecha, bg="#141E27")
 
-    def dibujar_flecha_izquierda(self, coordenada_x: int, coordenada_y: int):
+        if imagen is not None:
+            flecha.config(image=imagen)
+        
+        flecha.pack()
+
+    def dibujar_flecha_izquierda(self, coordenada_y: int, coordenada_x: int, imagen=None):
         frame_buscado = self.matriz_frames[coordenada_y + 1][coordenada_x - 1]
-        Label(frame_buscado, image=self.diagonal_izquierda, bg="#141E27").pack()
+        flecha = Label(frame_buscado, image=self.diagonal_izquierda, bg="#141E27")
+
+        if imagen is not None:
+            flecha.config(image=imagen)
+        
+        flecha.pack()
         
 
 root = Tk()
