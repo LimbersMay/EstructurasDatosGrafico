fc9b7ff0a2fe72814ec5a9309929cbef2d7239d9
Limbers <limbermay1@gmail.com>
2022-04-17 15:35:17 -0500

    Se agregaron los métodos de dibujar_flecha_izquierda y agregar_flecha_derecha

--- arbol_canvas.py
+++ arbol_canvas.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from estructuras.binary_tree import BinaryTree
-import uuid
+from PIL import ImageTk, Image
 
 
 class Arbol(Frame):
@@ -15,6 +15,10 @@ class Arbol(Frame):
 
         self.filas = 9
         self.columnas = 17
+
+        # Imagenes de las flechas
+        self.diagonal_derecha = ImageTk.PhotoImage(Image.open("recursos/derecha.png").resize((40, 40)))
+        self.diagonal_izquierda = ImageTk.PhotoImage(Image.open("recursos/izquierda.png").resize((40, 40)))
 
         self.arbol = BinaryTree(5)
 
@@ -61,77 +65,74 @@ class Arbol(Frame):
             nodos_nivel = self.arbol.level_nodes(nivel)
 
             if nivel == 0:
-                self.insertar_raiz(nivel, nodos_nivel)
+                self.dibujar_raiz(nivel, nodos_nivel)
 
             if nivel >= 1:
                 # Para los primeros hijos
-                self.insertar_nodos(nivel + contador, nivel + contador, nodos_nivel)
+                self.dibujar_nivel(nivel + contador, nivel + contador, nodos_nivel)
                 contador += 1
-
+            
     # Función para insertar el nodo raíz
-    def insertar_raiz(self, profundidad_y: int, nodo):
-        punto_medio_x = int(round(self.columnas / 2))
+    def dibujar_raiz(self, profundidad_y: int, nodo):
         
-        nodo_raiz = self.matriz_frames[profundidad_y][punto_medio_x]
+        punto_medio_x = int(round(self.columnas / 2))
+        self.dibujar_nodo(nodo[0], profundidad_y, 0)
 
-        # Datos del nodo
-        nodo_dato = nodo[0].get_data()
-        id_nodo = hex(id(nodo[0]))
+        # Pintamos 2 flechas en la diagonal derecha e izquierda
+        for i in range(1, 3):
+            diagonal_izquierda = self.matriz_frames[profundidad_y + i][punto_medio_x - i]
+            diagonal_derecha = self.matriz_frames[profundidad_y + i][punto_medio_x + i]
 
-        # Pintamos el nodo raíz
-        nodo_raiz.config(bg='#FFC107')
-
-        # Ponemos el dato del nodo raíz
-        Label(nodo_raiz, text=nodo_dato, bg='#FFC107').pack()
-        Label(nodo_raiz, text=id_nodo, bg='#FFC107', font=("Helvetica", 7)).pack()
-
-        # Ponemos las flechas
-
+            Label(diagonal_izquierda, image=self.diagonal_izquierda, bg="#141E27").pack()
+            Label(diagonal_derecha, image=self.diagonal_derecha, bg="#141E27").pack()
 
     # Función para insertar nodos
-    def insertar_nodos(self, profundidad_y: int, separacion_x: int, nodos: list):
-        punto_medio = int(round(self.columnas / 2))
-
+    def dibujar_nivel(self, profundidad_y: int, separacion_x: int, nodos: list):
         print("Nodos: ", nodos)
 
         # Por la cantidad de nodos que tiene el nivel
         for i in range(0, len(nodos) + 1, 4):
+
+            # Obtenemos el nodo izquierdo y derecho
+            nodo_izquierdo = nodos[0]
+            nodo_derecho = nodos[-1]
+
+            if nodo_izquierdo.get_data() is not None:
+                self.dibujar_nodo(nodo_izquierdo, profundidad_y, -separacion_x + i)
+                
+
+            if nodo_derecho.get_data() is not None:
+                self.dibujar_nodo(nodo_derecho, profundidad_y, separacion_x - i)
             
-            # Nodo derecho
-            # Comprobamos que el nodo derecho exista
-            if nodos[-1].get_data() is not None:
-                nodo_derecho_frame = self.matriz_frames[profundidad_y][punto_medio + separacion_x - i]
-                nodo_derecho_frame.config(bg='#FFC107')
-
-
-                # Datos del nodo
-                nodo_dato = nodos[-1].get_data()
-                id_nodo = hex(id(nodos[-1]))
-
-                # Label que contiene el dato del nodo
-                Label(nodo_derecho_frame, text=nodo_dato, bg='#FFC107').pack()
-
-                # Label que contiene el id del nodo
-                Label(nodo_derecho_frame, text=id_nodo, bg='#FFC107', font=("Helvetica", 7)).pack()
-            
-            # Nodo izquierdo
-            # Comprobamos que el nodo izquierdo exista
-            if nodos[0].get_data() is not None:
-                nodo_izquierdo_frame = self.matriz_frames[profundidad_y][punto_medio - separacion_x + i]
-                nodo_izquierdo_frame.config(bg='#FFC107')
-
-                # Datos del nodo
-                nodo_dato = nodos[0].get_data()
-                id_nodo = hex(id(nodos[0]))
-
-                # Label que contiene el dato del nodo
-                Label(nodo_izquierdo_frame, text=nodo_dato, bg='#FFC107').pack()
-                Label(nodo_izquierdo_frame, text=id_nodo, bg='#FFC107', font=("Helvetica", 7)).pack()
-
             # Eliminamos los nodos
             nodos.pop()
             nodos.pop(0)
+    
+    # Función para insertar UN NODO en la matriz
+    def dibujar_nodo(self, nodo, profundidad_y, separacion_x):
+        punto_medio = int(round(self.columnas / 2))
+        frame_buscado = self.matriz_frames[profundidad_y][punto_medio + separacion_x]
 
+        # Cambiamos de color al frame
+        frame_buscado.config(bg='#FFC107')
+
+        # Datos del nodo
+        nodo_dato = nodo.get_data()
+        id_nodo = hex(id(nodo))
+
+        # Label que contiene el dato del nodo
+        Label(frame_buscado, text=nodo_dato, bg='#FFC107').pack()
+        Label(frame_buscado, text=id_nodo, bg='#FFC107', font=("Helvetica", 7)).pack()
+
+
+    def dibujar_flecha_derecha(self, coordenada_x: int, coordenada_y: int):
+        frame_buscado = self.matriz_frames[coordenada_y + 1][coordenada_x + 1]
+        Label(frame_buscado, image=self.diagonal_derecha, bg="#141E27").pack()
+
+    def dibujar_flecha_izquierda(self, coordenada_x: int, coordenada_y: int):
+        frame_buscado = self.matriz_frames[coordenada_y + 1][coordenada_x - 1]
+        Label(frame_buscado, image=self.diagonal_izquierda, bg="#141E27").pack()
+        
 
 root = Tk()
 arbol = Arbol(root)
