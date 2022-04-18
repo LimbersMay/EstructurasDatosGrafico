91e69bac9c5f29adfe7a7123c53ff26c63bb07e7
Limbers <limbermay1@gmail.com>
2022-04-18 16:40:15 -0500

    Se agregó el apartado de información de la estructura del árbol binario de búsqueda

--- estructuras_opciones/arbol_busqueda_opciones.py
+++ estructuras_opciones/arbol_busqueda_opciones.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from estructuras.binary_search_tree import BinarySearchTree
+from .templates.inf_arbol_template import ArbolInformacion
 from .templates.botones_arbol_template import BotonesArbol
 from .templates.arbol_template import ArbolInterfaz
 
@@ -8,25 +9,43 @@ class ArbolBusquedaOpciones(Frame):
     def __init__(self, master):
         Frame.__init__(self, master)
 
+        # Estructura de dato que usaremos
+        self.arbol = BinarySearchTree()
+
         # Elementos del frame
         self.titulo = Label(self, text="Árbol de búsqueda")
-        self.arbol_interfaz = ArbolBusquedaInterfaz(self)
+        self.arbol_informacion = ArbolBusquedaInformacion(self, self.arbol)
+        self.arbol_interfaz = ArbolBusquedaInterfaz(self, self.arbol, self.arbol_informacion)
         self.botones_arbol = BotonesArbolBusqueda(self, self.arbol_interfaz)
 
-        # Posicionamos los elementos
+        # Posicionamiento de los elementos
         self.titulo.grid(row=0, column=0)
         self.arbol_interfaz.grid(row=1, column=0)
+        self.arbol_informacion.grid(row=1, column=1)
         self.botones_arbol.grid(row=2, column=0)
 
 
-class ArbolBusquedaInterfaz(ArbolInterfaz):
-    def __init__(self, master):
-        super().__init__(master)
+# Clase que mostrará toda la información del árbol binario
+class ArbolBusquedaInformacion(ArbolInformacion):
+    def __init__(self, master, arbol_busqueda):
+        super().__init__(master, arbol_busqueda)
 
-        self.arbol = BinarySearchTree()
+        # Posicionamos los elementos
+        self.titulo.grid(row=0, column=0, columnspan=2, sticky=W)
+        self.raiz.grid(row=1, column=0, sticky=W)
+        self.tamanio.grid(row=2, column=0, sticky=W)
+        self.profundidad.grid(row=3, column=0, sticky=W)
+
+
+class ArbolBusquedaInterfaz(ArbolInterfaz):
+    def __init__(self, master, arbol_busqueda, arbol_informacion):
+        super().__init__(master, arbol_busqueda, arbol_informacion)
     
     def insertar(self, elemento):
         self.arbol.insert(elemento)
+
+        # Dibujamos el árbol y actualizamos la información del frame
+        self.actualizar_informacion_frame()
         self.dibujar_arbol()
 
 
