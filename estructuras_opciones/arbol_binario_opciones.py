7fae2b5e7e5c6c490e0cd55b81f1a32b361c44fe
Limbers <limbermay1@gmail.com>
2022-04-18 16:33:49 -0500

    Se agregó el apartado de información del árbol binarrio

--- estructuras_opciones/arbol_binario_opciones.py
+++ estructuras_opciones/arbol_binario_opciones.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from estructuras.binary_tree import BinaryTree
+from .templates.inf_arbol_template import ArbolInformacion
 from .templates.arbol_template import ArbolInterfaz
 from .templates.botones_arbol_template import BotonesArbol
 
@@ -8,28 +9,47 @@ class ArbolBinarioOpciones(Frame):
     def __init__(self, master):
         Frame.__init__(self, master)
 
+        # Estructura utilizada
+        self.arbol = BinaryTree()
+
         master.title("Arbol binario")
 
         self.titulo = Label(self, text="Arbol Binario simple")
-        self.arbol_interfaz = ArbolBinarioInterfaz(self)
-        self.botones = BotonesArbolBinario(self, self.arbol_interfaz)
+        self.arbol_informacion = ArbolBinarioInformacion(self, self.arbol)
+        self.arbol_interfaz = ArbolBinarioInterfaz(self, self.arbol, self.arbol_informacion)
+        self.botones_arbol = BotonesArbolBinario(self, self.arbol_interfaz)
 
+        # Posicionamiento de los elementos
         self.titulo.grid(row=0, column=0)
         self.arbol_interfaz.grid(row=1, column=0)
-        self.botones.grid(row=2, column=0)
+        self.arbol_informacion.grid(row=1, column=1)
+        self.botones_arbol.grid(row=2, column=0)
+
+
+# Clase que mostrará toda la información del árbol binario
+class ArbolBinarioInformacion(ArbolInformacion):
+    def __init__(self, master, arbol_binario):
+        super().__init__(master, arbol_binario)
+
+        # Posicionamos los elementos
+        self.titulo.grid(row=0, column=0, columnspan=2, sticky=W)
+        self.raiz.grid(row=1, column=0, sticky=W)
+        self.tamanio.grid(row=2, column=0, sticky=W)
+        self.profundidad.grid(row=3, column=0, sticky=W)
+
 
 class ArbolBinarioInterfaz(ArbolInterfaz):
-    def __init__(self, master):
-        super().__init__(master)
-
-        self.arbol = BinaryTree()
+    def __init__(self, master, arbol_binario, arbol_informacion):
+        super().__init__(master, arbol_binario, arbol_informacion)
     
     def insertar_izquierda(self, elemento, referencia):
         self.arbol.insert_left(elemento, referencia)
+        self.actualizar_informacion_frame()
         self.dibujar_arbol()
 
     def insertar_derecha(self, elemento, referencia):
         self.arbol.insert_right(elemento, referencia)
+        self.actualizar_informacion_frame()
         self.dibujar_arbol()
 
 
