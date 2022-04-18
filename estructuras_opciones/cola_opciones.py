80a3e39a50a6558bb0d62d524ac6699a1618a766
Limbers <limbermay1@gmail.com>
2022-04-18 14:45:58 -0500

    Se agregó el apartado de la información de la estructura

--- estructuras_opciones/cola_opciones.py
+++ estructuras_opciones/cola_opciones.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from estructuras.cola import Cola
+from .templates.inf_estructura_template import EstructuraInformacion
 from .templates.lista_simple_template import ListaInterfaz
 from .templates.botones_lineales_template import BotonesBasicos
 
@@ -11,37 +12,73 @@ class ColaOpciones(Frame):
 
         master.title("Estructura de dato Cola")
 
+        # Estructura de dato que se utilizará en el programa
+        self.cola = Cola()
+
         # Atributos
         self.titulo = Label(self, text="Estructura de dato Cola")
-        self.cola_interfaz = ColaInterfaz(self)
+        self.cola_informacion = ColaInformacion(self, self.cola)
+        self.cola_interfaz = ColaInterfaz(self, self.cola, self.cola_informacion)
         self.botones_inferiores = BotonesCola(self, self.cola_interfaz)
 
         # Posicionamiento de los elementos
         self.titulo.grid(row=0, column=0)
         self.cola_interfaz.grid(row=1, column=0)
+        self.cola_informacion.grid(row=1, column=1)
         self.botones_inferiores.grid(row=2, column=0)
 
 
+# Clase que contiene toda la información de la cola
+class ColaInformacion(EstructuraInformacion):
+    
+    def __init__(self, master, estructura: Cola):
+        super().__init__(master, estructura)
+
+        self.estructura = estructura
+
+        self.maximo_variable = StringVar(self)
+
+        # Máximo de elementos de la cola
+        self.maximo = Label(self, textvariable=self.maximo_variable, bg="darkred", fg="white")
+
+        # Posicionamiento de los elementos
+        self.titulo.grid(row=0, column=0, sticky=W)
+
+        self.maximo.grid(row=1, column=0, sticky=W)
+        self.tamanio.grid(row=2, column=0, sticky=W)
+        self.tope.grid(row=3, column=0, sticky=W)
+        self.fondo.grid(row=4, column=0, sticky=W)
+
+        # Enviamos el maximo por defecto
+        self.set_maximo(0)
+    
+    # Método para actualizar el máximo de elementos de la cola
+    def set_maximo(self, maximo):
+        self.maximo_variable.set(f"Máximo: {maximo}")
+
+
+# Clase que contiene la interfaz de la cola
 class ColaInterfaz(ListaInterfaz):
 
-    def __init__(self, master):
-        super().__init__(master)
-
-        self.lista = Cola()
+    def __init__(self, master, estructura, estructura_informacion):
+        super().__init__(master, estructura, estructura_informacion)
 
     def insertar(self, elemento):
         self.lista.insertar(elemento)
 
+        self.actualizar_informacion()
         self.dibujar_lista()
 
     def eliminar(self):
         self.lista.eliminar()
-
+        
+        self.actualizar_informacion()
         self.dibujar_lista()
 
 
+# Clase que contiene los botones inferiores de la cola
 class BotonesCola(BotonesBasicos):
-    def __init__(self, master, cola_interfaz):
+    def __init__(self, master, cola_interfaz: ColaInterfaz):
         super().__init__(master, cola_interfaz)
 
         # Posicionamiento de los elementos del frame
