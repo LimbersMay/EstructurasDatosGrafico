29fb3c01a515251aab79526110750c23fcd0fe1b
Limbers <limbermay1@gmail.com>
2022-04-19 17:59:18 -0500

    Se refactorízó el código (espacios)

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from tkinter import *
 from ventana_principal import *
 
-def main():
 
+def main():
     root = Tk()
     ventana_principal = Ventana(root)
     ventana_principal.pack()
     root.mainloop()
+
 
 main()
